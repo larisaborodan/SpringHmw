b4b25b19a37701c79848e7a37e9e225563b2d430
Larisa <larisaborodan@gmail.com>
2025-12-17 19:02:20 +0200

    Added the database logic

--- src/main/java/org/launchcode/hello_spring/controllers/EventController.java
+++ src/main/java/org/launchcode/hello_spring/controllers/EventController.java
@@ -2,8 +2,10 @@ package org.launchcode.hello_spring.controllers;
 
 import jakarta.validation.Valid;
 import org.launchcode.hello_spring.data.EventData;
+import org.launchcode.hello_spring.data.EventRepository;
 import org.launchcode.hello_spring.models.Event;
 import org.launchcode.hello_spring.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,12 +18,13 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-
+    @Autowired
+    private EventRepository eventRepository;
 
     @GetMapping({ "", "/" })
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/index";
     }
 
@@ -39,14 +42,14 @@ public class EventController {
             model.addAttribute("title", "Create Event");
             return "events/create";
         }
-        EventData.add(newEvent);
+        eventRepository.save(newEvent);
         return "redirect:/events";
     }
 
     @GetMapping("delete")
     public String displayDeleteEventForm(Model model) {
         model.addAttribute("title", "Delete Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/delete";
     }
     @PostMapping("delete")
@@ -54,7 +57,8 @@ public class EventController {
 
         if(eventIds != null) {
             for (int id : eventIds) {
-                EventData.remove(id);
+                eventRepository.deleteById(id);
+
             }
         }
 
